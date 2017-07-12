654b62c9fccbd7bba0e9bd5940080c60c17618c2
Rodny Castro <rcastrom@ucenfotec.ac.cr>
2017-07-11 21:13:10 -0600

    Perfil

--- src/main/java/com/rammp/stretchyourbody/web/rest/MobileUserAppResource.java
+++ src/main/java/com/rammp/stretchyourbody/web/rest/MobileUserAppResource.java
@@ -1,7 +1,12 @@
 package com.rammp.stretchyourbody.web.rest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.codahale.metrics.annotation.Timed;
+import com.rammp.stretchyourbody.domain.UserApp;
+import com.rammp.stretchyourbody.service.UserAppService;
+import com.rammp.stretchyourbody.service.dto.UserAppDTO;
+import com.rammp.stretchyourbody.web.rest.util.HeaderUtil;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by mbp on 7/10/17.
@@ -9,4 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/app")
 public class MobileUserAppResource {
+
+    private final UserAppService userAppService;
+
+
+    public MobileUserAppResource(UserAppService userAppService) {
+        this.userAppService = userAppService;
+    }
+
+    @GetMapping("/user/{id}")
+    @Timed
+    public UserAppDTO getUser(@PathVariable Long id) {
+        return userAppService.findOne(id);
+    }
 }
